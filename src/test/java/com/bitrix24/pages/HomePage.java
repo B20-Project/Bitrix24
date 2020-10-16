a15ff504b9e7f3f7f2a19eb7c31987dc1686ed7b
Arpat <arpat9329@gmail.com>
2020-10-16 11:12:05 -0400

    Arpat2.0

--- src/test/java/com/bitrix24/pages/HomePage.java
+++ src/test/java/com/bitrix24/pages/HomePage.java
@@ -1,9 +1,11 @@
 package com.bitrix24.pages;
 
+import com.bitrix24.util.BrowserUtils;
 import com.bitrix24.util.Driver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class HomePage {
@@ -26,5 +28,65 @@ public class HomePage {
 
     @FindBy(xpath = "//div[@class='bx-desktop-appearance']")
     public WebElement sideWindowPopUp;
+
+    @FindBy(xpath = "//div[@class='bx-desktop-appearance-avatar']")
+    private WebElement sidePopUpWindow_contact_you;
+
+    @FindBy(xpath = "//div[@class='bx-desktop-appearance-avatar']")
+    public WebElement sidePopUpWindow_Verify_dialogMessenger;
+
+    @FindBy(xpath = "//div[@class='bx-desktop-tab-icon bx-desktop-tab-icon-im']")
+    private WebElement sidePopUpWindow_message;
+
+    @FindBy(xpath = "//div[@class='bx-desktop-tab bx-desktop-tab-notify']")
+    private WebElement sidePopUpWindow_notifications;
+
+    @FindBy(xpath = "//div[@class='bx-messenger-box-extra']")
+    public WebElement sidePopUpWindow_Verify_dialogNotifications;
+
+    @FindBy(xpath = "//div[@class='bx-desktop-tab-icon bx-desktop-tab-icon-config']")
+    private WebElement sidePopUpWindow_setting;
+
+    @FindBy(xpath = "//div[@id='bx-messenger-popup-settings']")
+    public WebElement sidePopUpWindow_Verify_popupSettings;
+
+    @FindBy(xpath = "//div[@class='bx-desktop-tab-icon bx-desktop-tab-icon-im-lf']")
+    private WebElement sidePopUpWindow_clones;
+
+
+    public void click_sidePopUpWindow_contact_you(){
+        WebDriverWait wait = new WebDriverWait(Driver.getDriver(), 10);
+        notificationIcon.click();
+        wait.until(ExpectedConditions.visibilityOf(sideWindowPopUp));
+        sidePopUpWindow_contact_you.click();
+    }
+
+    public void click_sidePopUpWindow_message(){
+        WebDriverWait wait = new WebDriverWait(Driver.getDriver(), 10);
+        notificationIcon.click();
+        wait.until(ExpectedConditions.visibilityOf(sideWindowPopUp));
+        sidePopUpWindow_message.click();
+    }
+
+    public void click_sidePopUpWindow_notifications(){
+        WebDriverWait wait = new WebDriverWait(Driver.getDriver(), 10);
+        search.click();
+        wait.until(ExpectedConditions.visibilityOf(sideWindowPopUp));
+        sidePopUpWindow_notifications.click();
+    }
+
+    public void click_sidePopUpWindow_setting(){
+        WebDriverWait wait = new WebDriverWait(Driver.getDriver(), 10);
+        notificationIcon.click();
+        wait.until(ExpectedConditions.visibilityOf(sideWindowPopUp));
+        sidePopUpWindow_setting.click();
+    }
+
+    public void click_sidePopUpWindow_clones(){
+        WebDriverWait wait = new WebDriverWait(Driver.getDriver(), 10);
+        notificationIcon.click();
+        wait.until(ExpectedConditions.visibilityOf(sideWindowPopUp));
+        sidePopUpWindow_clones.click();
+    }
 
 }
--- src/test/java/com/bitrix24/tests/TestHomePageTest.java
+++ src/test/java/com/bitrix24/tests/TestHomePageTest.java
@@ -2,14 +2,16 @@ package com.bitrix24.tests;
 
 import com.bitrix24.pages.HomePage;
 import com.bitrix24.util.BrowserUtils;
+import com.bitrix24.util.Driver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class TestHomePageTest extends AbstractTestBase {
 
-
     @Test(description = "click notification")
-    public void click_notification(){
+    public void verify_notification(){
         HomePage homePage = new HomePage();
         homePage.notificationIcon.click();
         BrowserUtils.wait(1);
@@ -18,7 +20,7 @@ public class TestHomePageTest extends AbstractTestBase {
     }
 
     @Test(description = "click search")
-    public void click_search(){
+    public void verify_search(){
         HomePage homePage = new HomePage();
         homePage.search.click();
         BrowserUtils.wait(1);
@@ -27,7 +29,7 @@ public class TestHomePageTest extends AbstractTestBase {
     }
 
     @Test(description = "click listOfCurrentContacted")
-    public void click_listOfCurrentContacted(){
+    public void verify_listOfCurrentContacted(){
         HomePage homePage = new HomePage();
         homePage.listOfCurrentContacted.click();
         BrowserUtils.wait(1);
@@ -36,11 +38,57 @@ public class TestHomePageTest extends AbstractTestBase {
     }
 
     @Test(description = "click bottomBlock and is not display")
-    public void click_bottomBlock(){
+    public void verify_bottomBlock(){
         HomePage homePage = new HomePage();
         homePage.bottomBlock.click();
+
+        Assert.assertFalse(homePage.sideWindowPopUp.isDisplayed());
+    }
+
+    @Test
+    public void verify_sidePopUpWindow_contact_you(){
+        HomePage homePage= new HomePage();
+        homePage.click_sidePopUpWindow_contact_you();
+        BrowserUtils.wait(1);
+
+        Assert.assertTrue(homePage.sidePopUpWindow_Verify_dialogMessenger.isDisplayed());
+    }
+
+    @Test
+    public void verify_sidePopUpWindow_message() {
+        HomePage homePage = new HomePage();
+        homePage.click_sidePopUpWindow_message();
+        BrowserUtils.wait(1);
+
+        Assert.assertTrue(homePage.sidePopUpWindow_Verify_dialogMessenger.isDisplayed());
+    }
+
+    @Test
+    public void verify_click_sidePopUpWindow_clones() {
+        HomePage homePage = new HomePage();
+        homePage.click_sidePopUpWindow_clones();
         BrowserUtils.wait(1);
 
         Assert.assertFalse(homePage.sideWindowPopUp.isDisplayed());
     }
+
+    @Test
+    public void verify_sidePopUpWindow_notifications() {
+        HomePage homePage = new HomePage();
+        homePage.click_sidePopUpWindow_notifications();
+        BrowserUtils.wait(1);
+
+        Assert.assertTrue(homePage.sidePopUpWindow_Verify_dialogNotifications.isDisplayed());
+    }
+
+    @Test
+    public void verify_sidePopUpWindow_setting() {
+        HomePage homePage = new HomePage();
+        homePage.click_sidePopUpWindow_setting();
+        BrowserUtils.wait(1);
+
+        Assert.assertTrue(homePage.sidePopUpWindow_Verify_popupSettings.isDisplayed());
+
+    }
+
 }
