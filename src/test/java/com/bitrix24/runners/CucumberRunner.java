d1f86e3f421f56b3b7afd4ddb75d8bfca5a9ae78
arpat9329 <68973655+arpat9329@users.noreply.github.com>
2020-11-03 11:51:46 -0500

    TaskAC1

--- src/test/java/com/bitrix24/runners/CucumberRunner.java
+++ src/test/java/com/bitrix24/runners/CucumberRunner.java
@@ -8,7 +8,7 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
                 features = "src/test/resources/features",
                 glue = "com/bitrix24/step_definitions",
-                tags = "@taskArpatAC",
+                tags = "@createTask",
                 dryRun = false
                 )
 
--- src/test/resources/features/Task.feature
+++ src/test/resources/features/Task.feature
@@ -1,6 +1,5 @@
 @createTask
-Feature: create task
-  As a user I want to create customized task
+Feature: As a user I want to create customized task
 
   Background: login
     Given user is on the home page
