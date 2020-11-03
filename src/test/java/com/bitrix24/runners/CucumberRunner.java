a585d2e7537ddcb9d31177dc9fc2c0d2a3423ba8
arpat9329 <68973655+arpat9329@users.noreply.github.com>
2020-11-03 18:16:22 -0500

    HTML Report

--- src/test/java/com/bitrix24/runners/CucumberRunner.java
+++ src/test/java/com/bitrix24/runners/CucumberRunner.java
@@ -6,6 +6,7 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
+                plugin = "json:target/cucumber.json",
                 features = "src/test/resources/features",
                 glue = "com/bitrix24/step_definitions",
                 tags = "@createTask",
